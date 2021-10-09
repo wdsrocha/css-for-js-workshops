2dbdd983899f4891400297d1de6669cc82b20502
Wesley Rocha <wesleysr1997@gmail.com>
2021-10-09 19:57:37 -0400

    Conclude exercise 04 from module 02

--- 02-rendering-logic-ii/src/App.js
+++ 02-rendering-logic-ii/src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
+      <div className="perspective-decoration" />
       <CharacterEditor />
       <Footer />
     </>
